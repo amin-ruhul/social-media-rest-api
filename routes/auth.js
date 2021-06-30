cb33ff60c32f4d65d88fff4e43d55ccae2bd5ce7
amin_ruhul <ruhulinfo50@gmail.com>
2021-06-30 12:50:03 +0600

    login user

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
+// register user
 router.post("/register", async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hasPassword = await bcrypt.hash(req.body.password, salt);
@@ -17,7 +18,24 @@ router.post("/register", async (req, res) => {
     const dbUser = await user.save();
     res.status(200).json(dbUser);
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+// login user
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).send("User Not Registered");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).send("Password not match");
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
