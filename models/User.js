b60f48b4eb75135d6e7088126bc7dfbea614520f
amin_ruhul <ruhulinfo50@gmail.com>
2021-07-21 22:28:41 +0600

    update user model

--- models/User.js
+++ models/User.js
@@ -9,21 +9,17 @@ const UserSchema = new mongoose.Schema(
       max: 20,
       unique: true,
     },
-
     email: {
       type: String,
-      require: true,
+      required: true,
       max: 50,
       unique: true,
     },
-
     password: {
       type: String,
-      require: true,
+      required: true,
       min: 6,
-      max: 30,
     },
-
     profilePicture: {
       type: String,
       default: "",
@@ -32,14 +28,33 @@ const UserSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
-
-    following: {
-      type: Array,
-      default: [],
-    },
     followers: {
       type: Array,
       default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 50,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+    from: {
+      type: String,
+      max: 50,
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
     },
   },
   { timestamps: true }
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const User = require("../models/User");
 
 const router = express.Router();
 
@@ -6,4 +7,13 @@ router.get("/", (req, res) => {
   res.send("hello from user");
 });
 
+router.post("/myprofile", (req, res) => {
+  const newUser = new User({
+    username: "",
+  });
+});
+
+router.post("/flow", (req, res) => {});
+
+router.post("/update", (req, res) => {});
 module.exports = router;
