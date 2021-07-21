5d9ad7af46316492abe33772ade534dc67879293
amin_ruhul <ruhulinfo50@gmail.com>
2021-07-21 22:32:49 +0600

    imp routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,41 +1,44 @@
-const express = require("express");
+const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-const router = express.Router();
-
-// register user
+//REGISTER
 router.post("/register", async (req, res) => {
-  const salt = await bcrypt.genSalt(10);
-  const hasPassword = await bcrypt.hash(req.body.password, salt);
-  const user = await new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: hasPassword,
-  });
-
   try {
-    const dbUser = await user.save();
-    res.status(200).json(dbUser);
-  } catch (error) {
-    res.status(500).json(error);
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-// login user
+//LOGIN
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).send("User Not Registered");
+    !user && res.status(404).json("user not found");
+
     const validPassword = await bcrypt.compare(
       req.body.password,
       user.password
     );
-    !validPassword && res.status(400).send("Password not match");
+    !validPassword && res.status(400).json("wrong password");
 
     res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
